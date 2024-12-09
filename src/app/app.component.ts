b074332ec6a61e004ee9660595b8fd41645694ba
logeshrxian <logeshdk02@gmail.com>
2024-12-10 00:14:20 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ImageComponent } from './image/image.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    ImageComponent,
+    SectionComponent,
+    FooterComponent 
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Forbes';
+  title = 'my-angular-app';
 }
---
+++ src/app/section/section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-section',
+  standalone: true, 
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css'],
+})
+export class SectionComponent {}
